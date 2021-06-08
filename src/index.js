98aab4f07b03088985ddb7f12d73adc5112f4473
JeUpark <genhcode@gmail.com>
2021-06-08 10:53:07 +0900

    #2.0 Vanilla ToDo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { createStore } from "redux";
 
+
+//--------------------< function Count >--------------------
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const number = document.getElementById("number");
 
 number.innerText = 0;
 
@@ -34,3 +36,35 @@ const handleMinus = () => countStore.dispatch({ type: MINUS });
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
+
+//--------------------< function To Do >--------------------
+const form = document.getElementById("toDo__form");
+const input = document.getElementById("toDo__input");
+const ul = document.getElementById("toDo__ul");
+
+const ADD_TODO= "ADD_TODO";
+const DELETE_TODO= "DELETE_TODO";
+
+const reducer = (state= [], action) => {
+  switch(action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  };
+};
+
+const store = createStore(reducer);
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({type: ADD_TODO, text: toDo});
+};
+
+form.addEventListener("submit", onSubmit);
+
+
